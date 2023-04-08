9ceeaf9067668b71ae3dcc76e340c73967a31a92
Trevor Colen <75765504+Trevasaures@users.noreply.github.com>
2023-04-07 22:08:52 -0500

    Update DecryptData_Student.cpp
    Some comments here and there just in case :3

--- ComputerOrganizationProject/DecryptData_Student.cpp
+++ ComputerOrganizationProject/DecryptData_Student.cpp
@@ -7,12 +7,19 @@
 
 void decryptData_01(char* data, int sized)
 {
+	// Setup to create global variables if needed in the future for debugging
+	// Two global variables to compare string information when searching for the decryption 
+	// globalDebug1 = 0;
+	// globalDebug2 = 0;
+	
 	__asm
 	{
 		// Zero the accumulator register
 		xor eax, eax;
 
 		// Fetch Global Variable data
+		// lea or load effective address so that our hash function has a point on the base register
+		// the hash function will serve as a way to keep track of and narrow down our search 
 		lea ebx, gPasswordHash;
 		mov esi, gNumRounds;
 
@@ -25,6 +32,7 @@ void decryptData_01(char* data, int sized)
 		// eax = index = gPasswordHash[0 + round * 4] * 256 + gPasswordHash[1 + round * 4]
 
 		// Setting up loop values:
+		// The two pointers are used for scaling through the array of values. One to increment the place in the string and the other to check it.
 		mov edx, sized; // Stop Case
 		xor ecx, ecx; // Counter
 		mov esi, data; // Data Pointer
@@ -32,9 +40,11 @@ void decryptData_01(char* data, int sized)
 
 	DECRYPT:
 		// Check to see if the program has reached the end of the array.
+		// je being jumo if equal or a conditional jump in this case checking if the end of the array has been reached.
 		cmp edx, ecx;
 		je END;
-
+		
+		
 		// data[x] = data[x] ^ gkey[index]
 		mov bl, byte ptr[edi + eax];
 		mov bh, byte ptr[esi + ecx];
